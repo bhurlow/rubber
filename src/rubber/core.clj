20aa29caf37931096c30931087da86067e00f003
bhurlow <bhurlow@gmail.com>
2015-11-20 23:48:34 -0500

    issue getting changes feeds right

--- src/rubber/core.clj
+++ src/rubber/core.clj
@@ -1,7 +1,9 @@
 (ns rubber.core
   (:require [rethinkdb.query :as r]
             [taoensso.timbre :as timbre :refer (debug info error)]
-            [org.httpkit.client :as http]))
+            [org.httpkit.client :as http]
+            [clojure.core.async :refer (<! >! go go-loop thread 
+                                       take! <!! >!! chan close!)]))
 
 (def search-url "http://search:9200")
 
@@ -15,7 +17,61 @@
   (let [conn  (r/connect :host  "rethink")]
     (when conn (info "search connected ✓"))))
 
+(def db-conn (atom nil))
+
+(defn get-conn []
+  (if @db-conn
+    @db-conn
+    (let [conn (r/connect :host "rethink")]
+      (info "making db connection")
+      (reset! db-conn conn))))
+
+(defn index-doc [obj]
+  false)
+
+(defn test-insert [ins]
+  (-> (r/db "test")
+      (r/table "test")
+      (r/insert {:thingy ins})
+      (r/run (get-conn))))
+
+(defn watch-table []
+  (-> (r/db "test")
+      (r/table "test")
+      (r/changes)
+      (r/run (get-conn))))
+
+(defn pump [chan]
+  (let [cursor (watch-table)]
+    (thread
+      (let [realized (doall cursor)]
+        (while true
+               (>!! chan (first cursor)))))))
+
+(defn test-changes []
+  (let [feed (chan)
+        cursor (watch-table)]
+
+    (println "using cursor: " cursor)
+
+    (doseq [x (range 10)]
+         (do (println "inserting" x)
+             (println (test-insert (str "inserted" x)))))
+      
+      cursor))
+
+
+    ; (go-loop []
+    ;          (>! feed chan)
+    ;          (recur))
+
+    ; (go-loop []
+    ;          (println "CHABNGE" (<! feed))
+    ;          (recur))
+
 (defn -main [& args]
+  (check-search)
+  (check-db)
   (println "SUP"))
 
 
